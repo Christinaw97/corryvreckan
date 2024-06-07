c110775e20c27a3bcc4115fb3ac5b58e070c25ef
Christian Bespin <cbespinv@cern.ch>
2024-06-07 13:57:50 +0200

    Working hdf5 event loader

---
+++ src/modules/EventLoaderHDF5/EventLoaderHDF5.h
@@ -0,0 +1,94 @@
+/**
+ * @file
+ * @brief Definition of module EventLoaderHDF5
+ *
+ * @copyright Copyright (c) 2017-2020 CERN and the Corryvreckan authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef EventLoaderHDF5_H
+#define EventLoaderHDF5_H 1
+
+#include <TH1F.h>
+#include <TH2F.h>
+#include <H5Cpp.h>
+#include <TGraph.h>
+#include "core/module/Module.hpp"
+#include "objects/Pixel.hpp"
+#include <queue>
+
+namespace corryvreckan {
+
+    class EventLoaderHDF5 : public Module {
+
+    public:
+        // Constructors and destructors
+        EventLoaderHDF5(Configuration& config, std::shared_ptr<Detector> detector);
+        ~EventLoaderHDF5() {}
+
+        // Standard algorithm functions
+        void initialize() override;
+        StatusCode run(const std::shared_ptr<Clipboard>& clipboard) override;
+
+    private:
+        struct Hit {
+            int column;
+            int row;
+            int charge;
+            unsigned long long timestamp;
+            unsigned int trigger_number;
+        };
+
+        using HitVector = std::vector<std::shared_ptr<Hit>>;
+
+        std::string m_fileName;
+        std::string m_datasetName;
+        double m_eventLength;
+        std::shared_ptr<Detector> m_detector;
+        hsize_t m_bufferSize;
+        bool m_sync_by_trigger;
+        double m_timestampShift;
+        int m_triggerShift;
+
+        H5::DataSet m_dataset;
+        H5::H5File m_file;
+        H5::DataSpace f_dataspace;
+        hsize_t f_total_records;
+        hsize_t m_start_record;
+
+        // Plots
+        TH2F* hHitMap;
+        TH1F* hPixelToT;
+        TH1D* hClipboardEventStart;
+        TH1D* hClipboardEventStart_long;
+        TH1D* hClipboardEventEnd;
+        TH1D* hClipboardEventDuration;
+
+        long long int m_currentEvent;
+
+        // Additional helper function
+        std::vector<Hit> readChunk();
+        bool loadData(const std::shared_ptr<Clipboard>& clipboard, PixelVector&);
+        void fillBuffer();
+        Event::Position getPosition(const std::shared_ptr<Clipboard>& clipboard);
+
+        // Sort buffer by timestamp to make sure to read them in chronological order.
+        // If timestamps are not available, sort by trigger number. If that fails, good luck
+        template <typename T> struct CompareTimeGreater {
+            bool operator()(const std::shared_ptr<T> a, const std::shared_ptr<T> b) {
+                if ((a->timestamp > 0) && (b->timestamp > 0)) {
+                    return a->timestamp > b->timestamp;
+                } else {
+                    return a->trigger_number > b->trigger_number;
+                }
+            }
+        };
+        std::priority_queue<std::shared_ptr<Hit>, HitVector, CompareTimeGreater<Hit>> m_buffer;
+    };
+
+} // namespace corryvreckan
+
+#endif // EventLoaderHDF5_H
---
+++ src/modules/EventLoaderHDF5/README.md
@@ -0,0 +1,42 @@
+---
+# SPDX-FileCopyrightText: 2017-2023 CERN and the Corryvreckan authors
+# SPDX-License-Identifier: CC-BY-4.0 OR MIT
+---
+# EventLoaderHDF5
+**Maintainer**: Christian Bespin (<cbespin@uni-bonn.de>)
+**Module Type**: *DETECTOR*  
+**Status**: Work in progress
+
+### Description
+This module loads data from hdf5 files and adds it to the clipboard. The input file must have extension `.h5` without any compression filters and follow the structure below:
+
+| column |  row  | charge |       timestamp      | trigger_number |
+|:------:|:-----:|:------:|:--------------------:|:--------------:|
+| `int`  | `int` | `int`  | `unsigned long long` | `unsigned int` |
+
+Decimal values for the charge are not supported yet and both raw and charge of the `Pixel` object are populated with the same value.
+
+The module is capable of defining an event as well as adding records based on timestamp or trigger. In case of the latter, trigger information must be present in the events.
+
+### Parameters
+* `filename`: Input file name.
+* `dataset_name`: Name of the node in the hdf5 file.
+* `buffer_size`: Buffer size for chunking.
+* `event_length`: Duration of the event if this module is the first event loader and defines the event. Ignored otherwise.
+* `sync_by_trigger`: Add records to the clipboard based on its trigger instead of timestamp. This requires an event definition with trigger information and can therefore not be used as first event loader.
+* `timestamp_shift`: Shift the timestamp of the record by the defined value in nanoseconds.
+* `trigger_shift`: Shift the trigger of the record by the defined value.
+
+### Plots produced
+
+The following plots are produced:
+
+* 2D map of pixel positions
+* Histogram with pixel charge
+
+### Usage
+```toml
+[EventLoaderHDF5]
+input_directory = "path/to/file"
+dataset_name = "Hits"
+```
