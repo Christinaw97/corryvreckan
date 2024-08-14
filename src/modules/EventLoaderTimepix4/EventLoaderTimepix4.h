de16df11ed2729284dffc083a2b9369faa88a201
Uwe Kraemer <ukraemer@nikhef.nl>
2024-08-14 11:49:52 +0200

    Actually had some leftover code in the dll correction, was removed now

--- src/modules/EventLoaderTimepix4/EventLoaderTimepix4.h
+++ src/modules/EventLoaderTimepix4/EventLoaderTimepix4.h
@@ -193,7 +193,7 @@ namespace corryvreckan {
 
         // Corrects latency delay due to DDLL clock distribution. Units are period of 40MHz (25ns)
         uint64_t toa_clkdll_correction(uint64_t spgroup_addr = 0) {
-	  return 15 - spgroup_addr << 2;
+            return spgroup_addr << 2;
         }
 
         // address including pixel, super pixel and super pixel group values
