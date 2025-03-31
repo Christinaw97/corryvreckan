4e4258ae2e9ca3dbd4b64a5d906915fae15f54ed
Pranav Manoj <pmanoj@nakedsnail.dhcp.lbl.gov>
2025-03-31 13:22:14 -0700

    Added some formatting changes

--- src/modules/EventLoaderYarr/EventLoaderYarr.h
+++ src/modules/EventLoaderYarr/EventLoaderYarr.h
@@ -9,18 +9,18 @@
  * SPDX-License-Identifier: MIT
  */
 
- #include <TCanvas.h>
- #include <TH1F.h>
- #include <TH2F.h>
- #include <thread>
- #include <chrono>
- #include <iostream>
- #include <queue>
- #include <dirent.h>
  #include "core/module/Module.hpp"
  #include "objects/Cluster.hpp"
  #include "objects/Pixel.hpp"
  #include "objects/Track.hpp"
+ #include <TCanvas.h>
+ #include <TH1F.h>
+ #include <TH2F.h>
+ #include <chrono>
+ #include <dirent.h>
+ #include <iostream>
+ #include <queue>
+ #include <thread>
  
  namespace corryvreckan {
      /** @ingroup Modules
@@ -56,18 +56,17 @@
          void finalize(const std::shared_ptr<ReadonlyClipboard>& clipboard) override;
  
      private:
-         
          // Timing-related member variables:
-         int event_number_;                                             // Event number
-         bool trigger_tag_timing_;                                      // Toggle for using the tag time
-         double previous_time_ = 0;                                     // Previous event time
-         int day_offset_ = 0;                                           // Offset for the day rollover
+         int event_number_;         // Event number
+         bool trigger_tag_timing_;  // Toggle for using the tag time
+         double previous_time_ = 0; // Previous event time
+         int day_offset_ = 0;       // Offset for the day rollover
  
          // Input file-related member variables:
-         std::string input_directory_;                                  // Input directory with .raw files
-         std::shared_ptr<Detector> detector_;                           // Detector pointer
-         std::string file_name_;                                        // Name of the raw file
-         std::fstream file_handle_;                                     // File stream for binary reading
+         std::string input_directory_;        // Input directory with .raw files
+         std::shared_ptr<Detector> detector_; // Detector pointer
+         std::string file_name_;              // Name of the raw file
+         std::fstream file_handle_;           // File stream for binary reading
  
          // Helper struct to hold header information
          struct TriggerHeader {
@@ -77,37 +76,25 @@
              uint16_t numHits;
              double time;
          };
-         
-         // Root Plots
-         TH2F* hHitMap;                                                  // 2D hitmap for each detector plane
-         TH1F* hEventsVsTagTime;                                         // Number of events vs time
-         TH1F* hNumHitsVsTagTime;                                        // Number of hits vs time
  
-
+         // Root Plots
+         TH2F* hHitMap;           // 2D hitmap for each detector plane
+         TH1F* hEventsVsTagTime;  // Number of events vs time
+         TH1F* hNumHitsVsTagTime; // Number of hits vs time
+ 
          // ----------------------------------- Helper Functions  -----------------------------------:
  
          // Read the header of the event which contains timing and trigger window information
          TriggerHeader read_header();
-         
-         // Read the hits (Col, Row, ToT) in an event and creates Pixel objects
-         void read_hits(
-             PixelVector& pixels,
-             double eventTime,
-             uint16_t nHits
-         );
-         
-         // Find the raw data files in the input directory
-         std::string find_raw_file(
-             const std::string& directory, 
-             const std::string& detectorName
-         );
-        
-         // Adjust the timestamp to account for day rollover
-         double adjust_time(
-             const double& current_time
-         );
  
+         // Read the hits (Col, Row, ToT) in an event and creates Pixel objects
+         void read_hits(PixelVector& pixels, double eventTime, uint16_t nHits);
+ 
+         // Find the raw data files in the input directory
+         std::string find_raw_file(const std::string& directory, const std::string& detectorName);
+ 
+         // Adjust the timestamp to account for day rollover
+         double adjust_time(const double& current_time);
      };
  
  } // namespace corryvreckan
- 
