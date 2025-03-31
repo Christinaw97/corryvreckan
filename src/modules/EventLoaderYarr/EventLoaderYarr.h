41cbc4c1587d4878081c164a050be397e150d16e
Pranav Manoj <pmanoj@nakedsnail.dhcp.lbl.gov>
2025-03-31 13:42:57 -0700

    Formatting fixes

--- src/modules/EventLoaderYarr/EventLoaderYarr.h
+++ src/modules/EventLoaderYarr/EventLoaderYarr.h
@@ -3,9 +3,10 @@
  * @brief Definition of module EventLoaderYarr
  *
  * @copyright Copyright (c) 2015-2024 CERN and the Corryvreckan authors.
- * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
- * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
- * Intergovernmental Organization or submit itself to any jurisdiction.
+ * This software is distributed under the terms of the MIT License, copied
+ * verbatim in the file "LICENSE.md". In applying this license, CERN does not
+ * waive the privileges and immunities granted to it by virtue of its status as
+ * an Intergovernmental Organization or submit itself to any jurisdiction.
  * SPDX-License-Identifier: MIT
  */
 
@@ -26,27 +27,32 @@
      /** @ingroup Modules
       * @brief Module to load ATLAS ITkPixV2 YARR events from a raw file.
       *
-      * This module reads a .raw file from a specified input directory (and detector name)
-      * and extracts trigger and hit data to be added to the clipboard. It also creates ROOT plots.
+      * This module reads a .raw file from a specified input directory (and detector
+      * name) and extracts trigger and hit data to be added to the clipboard. It also
+      * creates ROOT plots.
       */
      class EventLoaderYarr : public Module {
  
      public:
          /**
-          * @brief Constructor: retrieves configuration values (input directory, trigger multiplicity)
-          * @param config Configuration object for this module as retrieved from the steering file
+          * @brief Constructor: retrieves configuration values (input directory,
+          * trigger multiplicity)
+          * @param config Configuration object for this module as retrieved from the
+          * steering file
           * @param detector Pointer to the detector for this module instance
           */
          EventLoaderYarr(Configuration& config, std::shared_ptr<Detector> detector);
          ~EventLoaderYarr() {}
  
          /**
-          * @brief [Initialise this module] : Look for the data file, open it and initialise histograms
+          * @brief [Initialise this module] : Look for the data file, open it and
+          * initialise histograms
           */
          void initialize() override;
  
          /**
-          * @brief [Run the function of this module] : Process a group of trigger events and update the clipboard accordingly
+          * @brief [Run the function of this module] : Process a group of trigger
+          * events and update the clipboard accordingly
           */
          StatusCode run(const std::shared_ptr<Clipboard>& clipboard) override;
  
@@ -82,9 +88,10 @@
          TH1F* hEventsVsTagTime;  // Number of events vs time
          TH1F* hNumHitsVsTagTime; // Number of hits vs time
  
-         // ----------------------------------- Helper Functions  -----------------------------------:
+         // Helper Functions
  
-         // Read the header of the event which contains timing and trigger window information
+         // Read the header of the event which contains timing and trigger window
+         // information
          TriggerHeader read_header();
  
          // Read the hits (Col, Row, ToT) in an event and creates Pixel objects
@@ -98,3 +105,4 @@
      };
  
  } // namespace corryvreckan
+ 
