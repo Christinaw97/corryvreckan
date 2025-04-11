0138dbfd5ad88762aa992798c2f8cb97bc91fc87
Uwe Kraemer <ukraemer@nikhef.nl>
2025-04-11 16:54:27 +0200

    Added auxiliary devices into correlations. Only adding the timing correlation for these devices

--- src/modules/Correlations/Correlations.h
+++ src/modules/Correlations/Correlations.h
@@ -19,6 +19,7 @@
 #include "core/module/Module.hpp"
 #include "objects/Cluster.hpp"
 #include "objects/Pixel.hpp"
+#include "objects/TimerSignal.hpp"
 
 namespace corryvreckan {
     /** @ingroup Modules
