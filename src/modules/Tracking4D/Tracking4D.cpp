9330ec5920d411b1bc2b12e7a901567376fb3fcd
Uwe Kraemer <ukraemer@nikhef.nl>
2025-04-22 14:21:36 +0200

    Fixed the case that timestamp_from is not a parameter specified

--- src/modules/Tracking4D/Tracking4D.cpp
+++ src/modules/Tracking4D/Tracking4D.cpp
@@ -75,7 +75,7 @@ Tracking4D::Tracking4D(Configuration& config, std::vector<std::shared_ptr<Detect
     reject_by_ROI_ = config_.get<bool>("reject_by_roi");
     unique_cluster_usage_ = config_.get<bool>("unique_cluster_usage");
     exclude_auxiliary_ = config_.get<bool>("exclude_auxiliary");
-    if (get_detector(timestamp_from_)->isAuxiliary()){
+    if (!timestamp_from_.empty() && get_detector(timestamp_from_)->isAuxiliary()){
         LOG(DEBUG) << "Overwriting auxiliary exclusion as it was requested for the timestamp";
         exclude_auxiliary_ = false;
     }
