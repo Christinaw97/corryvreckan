637ffa1e383e2d1300e14d30b3b8b9c70ca68fd4
christina <christiw@fnal.gov>
2025-07-16 11:18:48 -0500

    update script and geo for alignment

--- script.sh
+++ script.sh
@@ -10,4 +10,11 @@ cp /eos/uscms/store/group/cmstestbeam/${BASE_PATH}/Tracks/RawData/run${run_numbe
 
 bin/corry -c ${config}
 cp output/TreeWriter/output.root /eos/uscms/store/user/cmstestbeam/${BASE_PATH}/Tracks/RecoData/v1/Run${1}_CMSTiming_FastTriggerStream_converted.root
+
+#make plots
+cd plot_scripts
+./make_plots.sh ../output/out_MIMOSA.root Run${1}
+cd ../
+
+#rm ROOT files
 rm -rf output
